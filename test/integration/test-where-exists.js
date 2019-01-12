6d4cfbf3a39cd4a1d4cfc5f27427bf6d8bb12b10
ChanRay <richardo2016@gmail.com>
2019-01-13 04:19:47 +0800

    add test case about where-exists.

--- test/integration/test-where-exists.js
+++ test/integration/test-where-exists.js
@@ -15,6 +15,21 @@ describe('where-exists', () => {
       common.Select().from('table1').whereExists('table2', 'table1', [['fid1', 'fid2'], ['id1', 'id2']], { col1: 1, col2: 2 }).build(),
       'SELECT * FROM `table1` `t1` WHERE EXISTS (SELECT * FROM `table2` WHERE `fid1` = `t1`.`id1` AND `fid2` = `t1`.`id2` AND `col1` = 1 AND `col2` = 2)'
     )
+
+    assert.equal(
+      common.Select().from('table1').whereExists(
+		  	'table2', 'table1',
+		  	[
+			  ['fid1', 'fid2'],
+			  ['id1', 'id2']
+			],
+			{
+				col1: common.Query.ne(1),
+				col2: common.Query.gte(2)
+			}
+		).build(),
+      'SELECT * FROM `table1` `t1` WHERE EXISTS (SELECT * FROM `table2` WHERE `fid1` = `t1`.`id1` AND `fid2` = `t1`.`id2` AND `col1` <> 1 AND `col2` >= 2)'
+    )
   })
 })
 
